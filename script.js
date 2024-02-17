9d38105aa55d06cd99c82bcbe2d9d83d85d41332
SteveRobert7 <141488857+SteveRobert7@users.noreply.github.com>
2024-02-17 13:28:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/** @format */
+
 
 let arr = [
   { id: 1, name: "john", age: "18", marks: 80 },
@@ -7,21 +7,37 @@ let arr = [
 ];
 
 function PrintStudentswithMap() {
-  //Write your code here , just console.log
+  const studentsOver50 = arr
+    .filter((student) => student.marks > 50)
+    .map((student) => console.log(student));
 }
 
 function PrintStudentsbyForEach() {
-  //Write your code here , just console.log
+  arr.forEach((student) => {
+    if (student.marks > 50) {
+      console.log(student);
+    }
+  });
 }
 
 function addData() {
-  //Write your code here, just console.log
+  const newStudent = { id: 4, name: "susan", age: "20", marks: 45 };
+  arr.push(newStudent);
+  console.log(newStudent);
 }
 
 function removeFailedStudent() {
-  //Write your code here, just console.log
+  const passedStudents = arr.filter((student) => student.marks >= 50);
+  console.log(passedStudents);
 }
 
 function concatenateArray() {
-  //Write your code here, just console.log
+  const newArray = [
+    { id: 4, name: "Japan", age: "22", marks: 49 },
+    { id: 5, name: "Afin", age: "24", marks: 55 },
+    { id: 6, name: "Ottakam", age: "21", marks: 61 },
+  ];
+
+  const concatenatedArray = arr.concat(newArray);
+  console.log(concatenatedArray);
 }
